258a53de0921a21631edb607623d600cbcb90222
YulliErisna <167390598+YulliErisna@users.noreply.github.com>
2025-12-10 10:42:08 +0700

    Create logger.ts

---
+++ lib/logger.ts
@@ -0,0 +1,9 @@
+// lib/logger.ts
+import pino from "pino";
+
+const logger = pino({
+  prettyPrint: process.env.NODE_ENV !== "production",
+  level: process.env.LOG_LEVEL || "info",
+});
+
+export default logger;
