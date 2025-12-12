fd90e74c44a38c6d869b0b137fd97ae9b8debfda
Rahmatun Nisa <rhtn23@mhs.usk.ac.id>
2025-12-12 10:58:51 +0700

    Remove unwanted files

--- lib/logger.ts
+++
@@ -1,9 +0,0 @@
-// lib/logger.ts
-import pino from "pino";
-
-const logger = pino({
-  prettyPrint: process.env.NODE_ENV !== "production",
-  level: process.env.LOG_LEVEL || "info",
-});
-
-export default logger;
